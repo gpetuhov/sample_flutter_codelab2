a0f6eb1b929840988216f2b45aaa1f65fccf57e1
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-14 16:13:35 +0300

    Add chat message

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,11 @@ class ChatScreenState extends State<ChatScreen> {
     // to set color of the send button.
     return IconTheme(
       // This changes color of send button icon to the accent color of the app's theme.
+      // A BuildContext object is a handle to the location of a widget in your app's widget tree.
+      // Each widget has its own BuildContext, which becomes the parent of the widget returned
+      // by the StatelessWidget.build or State.build function.
+      // This means the _buildTextComposer() method can access the BuildContext object
+      // from its encapsulating State object; you don't need to pass the context to the method explicitly.
       data: IconThemeData(color: Theme.of(context).accentColor),
       // Container widget adds a horizontal margin between
       // the edge of the screen and each side of the input field
@@ -52,8 +57,10 @@ class ChatScreenState extends State<ChatScreen> {
               // TextField handles the mutable text content
               child: TextField(
                 controller: _textController,
-                onSubmitted: _handleSubmitted,  // this is triggered when pressing OK on the keyboard
-                decoration: InputDecoration.collapsed(hintText: "Send a message"),
+                onSubmitted:
+                    _handleSubmitted, // this is triggered when pressing OK on the keyboard
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message"),
               ),
             ),
             // We place send button inside the Container to specify margins
@@ -63,8 +70,7 @@ class ChatScreenState extends State<ChatScreen> {
                   icon: Icon(Icons.send),
                   // This is triggered on button pressed.
                   // TextEditingController keeps entered text.
-                  onPressed: () => _handleSubmitted(_textController.text)
-              ),
+                  onPressed: () => _handleSubmitted(_textController.text)),
             ),
           ],
         ),
@@ -77,3 +83,44 @@ class ChatScreenState extends State<ChatScreen> {
     _textController.clear();
   }
 }
+
+class ChatMessage extends StatelessWidget {
+
+  ChatMessage({this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // For the avatar, the parent is a Row widget whose main axis is horizontal,
+          // so CrossAxisAlignment.start gives it the highest position along the vertical axis.
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // For messages, the parent is a Column widget whose main axis is vertical,
+              // so CrossAxisAlignment.start aligns the text at the furthest left position
+              // along the horizontal axis.
+              // Using current theme allows us to avoid hard-coding font sizes and other text attributes.
+              Text(_name, style: Theme.of(context).textTheme.subhead),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _name = "User Name";
